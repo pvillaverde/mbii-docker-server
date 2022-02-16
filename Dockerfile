71b555e9fba3e29a3cdf7f321667fd4f85d18139
pvillaverde <pvillaverde@qualigy.com>
2022-02-16 13:01:27 +0100

    Initial Commit [MB2+RTV]

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu
+LABEL MAINTAINER="Fedello Kirfed <fedellokirfed@gmail.com>"
+
+# Expose a range of possible Jedi Academy ports.
+EXPOSE 29060-29062/udp 29070-29081/udp
+
+# 32bit Arch
+RUN dpkg --add-architecture i386
+
+# Install dependencies.
+RUN apt-get update 
+RUN apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
+RUN apt-get install -y curl:i386 
+RUN apt-get install -y python-setuptools python-dev 
+RUN apt-get install -y wget unzip
+
+# Server Files
+COPY server/* /opt/openjk/
+
+# LIB FILES
+COPY server/libcxa.so.1 /usr/lib/
+RUN chmod a+r /usr/lib/libcxa.so.1
+
+# Set the working directory to where the Jedi Academy data files will be
+# mounted at, so that linuxjampded finds them.
+
+WORKDIR /opt/openjk
+
+# Start the server.
+RUN chmod +x /opt/openjk/linuxjampded
+RUN chmod +x /opt/openjk/start.sh
+
+CMD ["/opt/openjk/start.sh"]
---
+++ server/start.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+######### VARIABLES (If Undefined)
+: "${GAME_ENGINE:=linuxjampded}"
+: "${SERVER_CONFIG:=server_config_default.cfg}"
+: "${SERVER_PORT:=29070}"
+: "${RTVRTM_CONFIG:=}"
+
+######### Colors
+RESTORE='\033[0m'
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+BLUE='\033[00;34m'
+
+echo-green() {
+	dt=$(date '+%d/%m/%Y %H:%M:%S')
+	echo -e "${dt}: ${GREEN}${1}${RESTORE}"
+}
+
+echo-red() {
+	dt=$(date '+%d/%m/%Y %H:%M:%S')
+	echo -e "${dt}: ${RED}${1}${RESTORE}"
+}
+
+echo-blue() {
+	dt=$(date '+%d/%m/%Y_%H:%M:%S')
+	echo -e "${dt}: ${BLUE}${1}${RESTORE}"
+}
+######### Download / Update RTVRTM
+mkdir /opt/rtvrtm/ &>/dev/null
+wget -O /opt/rtvrtm/RTVRTM.zip https://www.moviebattles.org/download/RTVRTM.zip
+unzip /opt/rtvrtm/RTVRTM.zip "Linux/*" -d "/opt/rtvrtm/" && mv /opt/rtvrtm/Linux/* /opt/rtvrtm/ && rmdir /opt/rtvrtm/Linux
+
+# If an rtvrtm configuration file has been defined and it exists, start rtvrtm.
+
+if [ -f "/opt/openjk/MBII/${RTVRTM_CONFIG}" ]; then
+	echo-green "Found RTV RTM Config at /opt/openjk/MBII/${RTVRTM_CONFIG}"
+
+	cp /opt/openjk/MBII/*.txt /opt/rtvrtm
+	cp "/opt/openjk/MBII/${RTVRTM_CONFIG}" /opt/rtvrtm/rtvrtm.cfg
+
+	echo-green "Starting RTV RTM Service"
+
+	until (
+		sleep 10
+		python /opt/rtvrtm/rtvrtm.py -c /opt/rtvrtm/rtvrtm.cfg --noupdate
+	); do
+		echo "RTVRTM crashed with exit code $?. Restarting..." >&2
+	done &
+else
+	echo-red "Unable to find RTV RTM Config at /opt/openjk/MBII/${RTVRTM_CONFIG}"
+fi
+
+#########
+# Run script to update your server to the current version (use argument "openjk" if using OpenJK engine).
+# Customize variables below as necessary.
+
+### Check for Updates on MBII on startup
+echo "**** Checking MBII Updates ****"
+cd /opt/openjk/MBII
+wget --inet4-only --execute="robots = off" -r -l 1 -nd -nH -N -A pk3,so,cfg,txt,mb2c,mbcr https://update.moviebattles.org/files/MBII/
+if [ "$GAME_ENGINE" == "openjk" ]
+then
+	mv -f jampgamei386.so jampgamei386.jamp.so
+	cp jampgamei386.nopp.so jampgamei386.so
+fi
+echo "***** Done! *****"
+cd /opt/openjk/
+## Start Sreve
+./$GAME_ENGINE +set fs_game MBII +set dedicated 2 +exec ${SERVER_CONFIG}
