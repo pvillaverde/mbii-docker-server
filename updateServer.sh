a9ab85bf6b8643ba42497c03390edec5d4cb5270
FedelloKirfed <fedellokirfed@proton.me>
2024-03-17 21:26:48 +0100

    👽️ Updateserver script

--- updateServer.sh
+++ updateServer.sh
@@ -4,21 +4,22 @@
 # Customize variables below as necessary.
 
 # directory where base and MBII folder reside (AKA GameData on clients)
-[ ! -d "MBII/" ] && echo "You must execute this script from the gamedata folder with the MBII subfolder." && exit 1
+basedir="/opt/openjk"
 
 read -p "Update MBII?" -n 1 -r
 echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 
    # download newer files
-   cd MBII
-   wget --execute="robots = off" -r -l 1 -nd -nH -N -A pk3,so,cfg,txt,mb2c,mbcr https://update.moviebattles.org/files/MBII/
-   if [ "$1" == "openjk" ]; then
+   cd $basedir
+   dotnet MBII_CommandLine_Update_XPlatform.dll
+
+   read -p "Is your engine OpenJK based?" -n 1 -r
+   if [[ $REPLY =~ ^[Yy]$ ]]; then
+      cd $basedir/MBII
       mv -f jampgamei386.so jampgamei386.jamp.so
       cp jampgamei386.nopp.so jampgamei386.so
    fi
-
-   # start servers
 
    echo "***** Done! *****"
 else
